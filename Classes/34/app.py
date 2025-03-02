a5d412536fcbace2aadd5e16278aaa04ee28ffad
IronKV <kaloev.valera@gmail.com>
2025-03-02 14:36:52 +0300

    add 34

---
+++ Classes/34/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    user_data = None
+    if request.method == 'POST':
+        # Получаем данные из формы
+        name = request.form.get('name')
+        city = request.form.get('city')
+        hobby = request.form.get('hobby')
+        age = request.form.get('age')
+        # Сохраняем данные в словарь
+        user_data = {
+            'name': name,
+            'city': city,
+            'hobby': hobby,
+            'age': age
+        }
+    return render_template('index.html', user_data=user_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
